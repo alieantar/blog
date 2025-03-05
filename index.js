5be2921a5c48ccfc4c11edb3f243f05c2d963439
Alie Antar <alieantar2795@outlook.com>
2025-03-05 14:35:32 -0500

    Code has been modified so that data is stored on the client side in sessionStorage. This way, posts get deleted when the tab or browser is closed.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 
-let posts = {};
+// let posts = {};
 let numPosts = 0;
 
 app.listen(port, () => {
@@ -25,29 +25,30 @@ app.get("/create", (req, res) => {
 
 app.get("/view", (req, res) => {
     
-    res.render("view.ejs", { posts: posts });
+    // Client-side so no need to pass data
+    res.render("view.ejs");
 });
 
-app.post("/submit", (req, res) => {
-    const dateTitleContent = [];
-    numPosts++;
-    const postId = `${(req.body.title).replace(/\s+/g, '-')}_${numPosts}`;
-    var postDate = new Date().toDateString();
-    postDate = postDate.slice(0, 3) + ', ' + postDate.slice(4, postDate.length);
-    const postTitle = req.body.title;
-    const postContent = req.body.content;
+// app.post("/submit", (req, res) => {
+//     const dateTitleContent = [];
+//     numPosts++;
+//     const postId = `${(req.body.title).replace(/\s+/g, '-')}_${numPosts}`;
+//     var postDate = new Date().toDateString();
+//     postDate = postDate.slice(0, 3) + ', ' + postDate.slice(4, postDate.length);
+//     const postTitle = req.body.title;
+//     const postContent = req.body.content;
 
-    // For development use.
-    console.log("Post ID: ", postId);
-    console.log("Date Posted: ", postDate);
-    console.log("Post Title: ", postTitle);
+//     // For development use.
+//     console.log("Post ID: ", postId);
+//     console.log("Date Posted: ", postDate);
+//     console.log("Post Title: ", postTitle);
     
-    if (postId && postDate && postTitle && postContent){
-        dateTitleContent.push(postDate, postTitle, postContent);
-        posts[postId] = dateTitleContent;
-        res.redirect("/view")
-    }
-    else{
-        res.send('<p>Nothing to submit!</p><a href="/">Go home.</a>');
-    }
-});
+//     if (postId && postDate && postTitle && postContent){
+//         dateTitleContent.push(postDate, postTitle, postContent);
+//         posts[postId] = dateTitleContent;
+//         res.redirect("/view")
+//     }
+//     else{
+//         res.send('<p>Nothing to submit!</p><a href="/">Go home.</a>');
+//     }
+// });
